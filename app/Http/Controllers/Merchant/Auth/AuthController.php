cf601a0c6893af26d4dca543aeab165fec372fc8
joy2362 <abdullahzahidjoy@gmail.com>
2023-05-07 19:08:31 +0600

    feat: merchant registration api added

--- app/Http/Controllers/Merchant/Auth/AuthController.php
+++ app/Http/Controllers/Merchant/Auth/AuthController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers\Merchant\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Merchant\RegistrationRequest;
+use App\Services\Merchant\Auth\AuthService;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct(public AuthService $service)
+    {
+    }
+
+    public function register(RegistrationRequest $request): JsonResponse
+    {
+        return apiResponse($this->service->register($request));
+    }
 }
