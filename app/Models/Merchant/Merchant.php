f4d905eee6a2f8af8be88506550c53188417a27e
joy2362 <joy2362@users.noreply.github.com>
2023-05-07 11:13:59 +0000

    style: fix style

--- app/Models/Merchant/Merchant.php
+++ app/Models/Merchant/Merchant.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
 class Merchant extends Authenticatable
 {
     use HasFactory;
