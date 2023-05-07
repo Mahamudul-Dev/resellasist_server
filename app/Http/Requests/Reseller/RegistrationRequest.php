94399dc1c77400e3dc30e579167b4bba11157846
joy2362 <abdullahzahidjoy@gmail.com>
2023-05-07 18:05:57 +0600

    feat: Add custom request class and publish lang

---
+++ app/Http/Requests/Reseller/RegistrationRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Reseller;
+
+use App\Http\Requests\FormRequest\CustomFormRequest;
+
+class RegistrationRequest extends CustomFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
