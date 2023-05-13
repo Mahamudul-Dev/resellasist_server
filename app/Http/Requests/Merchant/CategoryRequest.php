4b21d109e82202239693ae2880c678d68b98ccb5
joy2362 <abdullahzahidjoy@gmail.com>
2023-05-14 02:12:15 +0600

    fix: category request

--- app/Http/Requests/Merchant/CategoryRequest.php
+++ app/Http/Requests/Merchant/CategoryRequest.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Requests\Merchant;
 
+use App\Http\Requests\FormRequest\CustomFormRequest;
 use Illuminate\Foundation\Http\FormRequest;
 
-class CategoryRequest extends FormRequest
+class CategoryRequest extends CustomFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
